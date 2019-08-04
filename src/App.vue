ad9755aed811874777e324c27973aa29c23194de
magicstf <2641684677@qq.com>
2019-08-04 20:31:45 +0800

    使用element-ui和vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app">app</div>
+  <div id="app">
+    app
+    <el-button type="success">成功按钮</el-button>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// import router from '.router/index.js'
+// @是src路径的别名-在vue-cli文件下有效
+// index是目录索引文件  index.js index.vue index.json
+import router from '@/router'
+
 // 产品级别提示-不开启
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 // main.js 入口文件
 // 职责1：引入需要依赖的模块
 // 职责2：创建一个根Vue实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
