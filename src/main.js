163c608f6d0e891240b7a0096100f5c782d42906
magicstf <2641684677@qq.com>
2019-08-04 10:05:52 +0800

    ’初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 产品级别提示-不开启
 Vue.config.productionTip = false
 
+// main.js 入口文件
+// 职责1：引入需要依赖的模块
+// 职责2：创建一个根Vue实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
